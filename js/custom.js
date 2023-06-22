209c1f5da9ac8268b3e75e84944f26c63d38a448
j3ongjisu <wjdwltn911@naver.com>
2023-06-22 13:10:23 +0900

    20230622

--- js/custom.js
+++ js/custom.js
@@ -13,9 +13,9 @@ $(function () {
     const MainSlide = new Swiper('.main_slide', {
         loop: true,
         parallax: true,
-        speed: 1200,
+        speed: 1600,
         autoplay: {
-            delay: 3000,
+            delay: 2500,
             disableOnInteraction: false,
         },
         on: {
@@ -49,13 +49,20 @@ $(function () {
 
     const ItmSlide = new Swiper('.itm_slide', {
         loop: true,
-        slidesPerView: 2,
-        spaceBetween: 400,
+        slidesPerView: 1,
+        spaceBetween: 0,
         speed: 900,
         autoplay: {
             delay: 4000,
             disableOnInteraction: false,
         },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 400,
+            }
+        }
     });
 
     $('.MainItm .arrows .left').on('click', function () {
@@ -68,6 +75,22 @@ $(function () {
     // document.querySelector('.MainItm .arrows .left').addEventListener('click', () => {
     //     ItmSlide.slidePrev();
     // });
+
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.Gnb').toggleClass('on');
+    });
+
+    $('.Gnb>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).parent().siblings().find('.snb').stop().slideUp();
+    });
+
+    $(window).on('resize', function () {
+        $('.Gnb .snb').removeAttr('style')
+    })
+
 
 
 
